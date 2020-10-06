d60a9542959b0f49c83121c5343f2008d400d77d
atlas <robertdennispersonal@gmail.com>
2020-10-06 18:49:02 -0400

    prep for split project

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,21 +1,6 @@
 <?php
 
 /*
- * This file is part of Strix.
- *
- * For detailed copyright and license information, please view the
- * LICENSE file that was distributed with this source code.
- */
-
-
-/*
- * This file is part of Strix.
- *
- * For detailed copyright and license information, please view the
- * LICENSE file that was distributed with this source code.
- */
-
-/*
 |--------------------------------------------------------------------------
 | Create The Application
 |--------------------------------------------------------------------------
